e5a467046935ebca541bbb439a5c0058e37210b5
Nia Williams <niaaw605@gmail.com>
2021-07-01 12:03:18 -0400

    init commit for motivate app, useless app for INIT hackathon.

---
+++ app/src/main/java/com/niaapps/motivateapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.niaapps.motivateapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView main;
+    ImageButton happy;
+    String [] array;
+    int count =0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        main = findViewById(R.id.motivate);
+        happy = findViewById(R.id.imageButton2);
+        array =  getResources().getStringArray(R.array.messages);
+
+
+        happy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                main.setText(array[count]);
+                count++;
+
+                if(count >= 6){
+                    count = 0;
+                }
+            }
+        });
+    }
+
+}
